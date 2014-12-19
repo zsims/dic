04ffc0cfe3f633286d61e316bdff1899d312e7bc
Zachary Sims <zsims@users.noreply.github.com>
2014-12-19 20:17:03 +1100

    txt -> rst

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from distutils.core import setup
+import os
+
+with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+    long_description = readme.read()
+
+from dic import __version__
 
 setup(
     name='dic',
-    version='0.1.0',
+    version=__version__,
     author='Zachary Sims',
     author_email='zsims@users.noreply.github.com',
     packages=['dic', 'dic.test'],
@@ -10,6 +16,6 @@ setup(
     url='https://github.com/zsims/dic',
     license='LICENSE.txt',
     description='Inversion of Control micro-framework',
-    long_description=open('README.txt').read(),
+    long_description=long_description,
     install_requires=[],
 )
