09deb2591637e1f2e0b89612b06a6bfe62aaaf36
Zachary Sims <zsims@users.noreply.github.com>
2014-12-19 21:36:03 +1100

    Adds lazy() relationship

--- dic/rel.py
+++ dic/rel.py
@@ -14,11 +14,36 @@ class FactoryRelationship(Relationship):
 
 def factory(component_type):
     """
-    Models a factory relationship capable of creating the given type.
+    Models a factory relationship capable of creating the given type. The scope of the registered component is
+    respected. Meaning a SingleInstance registration will return the same instance for multiple factory calls.
     :param component_type:
     :return: A factory relationship
     """
     return FactoryRelationship(component_type)
 
 
+class LazyRelationship(Relationship):
+    def __init__(self, component_type):
+        super().__init__()
+        self.component_type = component_type
+        self._component = None
+
+    @property
+    def has_value(self):
+        return self._component is not None
+
+    @property
+    def value(self):
+        if self._component is None:
+            self._component = self._container.resolve(self.component_type)
+        return self._component
+
+
+def lazy(component_type):
+    """
+    Models a lazy relationship. Dependency lookup is delayed until the value is resolved for the first time.
+    :param component_type: The component type.
+    :return: A lazy relationship
+    """
+    return LazyRelationship(component_type)
 
--- dic/test/test_rel.py
+++ dic/test/test_rel.py
@@ -7,6 +7,14 @@ class Part(object):
         self.data = None
 
 
+class GuyWhoPutsUpWithLaziness(object):
+    def __init__(self, bomb: dic.rel.lazy(Part)):
+        self.lazy_part = bomb
+
+    def do_it(self, data):
+        self.lazy_part.value.data = data
+
+
 class SuperFactory(object):
     def __init__(self, part_factory: dic.rel.factory(Part)):
         self.part_factory = part_factory
@@ -41,6 +49,21 @@ class FactoryTestCase(unittest.TestCase):
         self.assertEqual(p1.data, 1)
         self.assertEqual(p2.data, 2)
         self.assertEqual(p3.data, 3)
+
+    def test_lazy_delays_resolve(self):
+        # Arrange
+        self.builder.register_class(Part)
+        self.builder.register_class(GuyWhoPutsUpWithLaziness)
+        container = self.builder.build()
+        guy = container.resolve(GuyWhoPutsUpWithLaziness)
+
+        self.assertFalse(guy.lazy_part.has_value)
+
+        # Act
+        guy.do_it("data")
+
+        # Assert
+        self.assertEqual("data", guy.lazy_part.value.data)
 
 if __name__ == '__main__':
     unittest.main()
